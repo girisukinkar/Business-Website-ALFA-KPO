04e4c36688f0aa1500b25f9b2ffcba3bb5591d2c
Girish Sukinkar <girisukinkar@gmail.com>
2017-09-03 03:40:46 +0530

    Done
    Contact Remaining

---
+++ contact.php
@@ -0,0 +1,83 @@
+<?php include'head.php'; ?>
+<?php include 'navigation.php'; ?>
+
+<div class="container">
+<br><br>
+
+
+	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15077.613661966581!2d72.8371691!3d19.1338107!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x68e16b69df50fc42!2sALFA+KPO+Pvt.+Ltd!5e0!3m2!1sen!2sin!4v1503764522138" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	</div>
+
+
+</div>
+<footer class="footer">
+
+<div class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>Alfa<span>KPO</span></h3>
+
+				<p class="footer-links">
+					High quality IT professionals at your service.
+				</p>
+
+				<p class="footer-company-name">Company Name &copy; 2017</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>301, Woodrow Business Park</span>Andheri West</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+1 555 123456</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">support@company.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>Follow us on Social Media</span>
+					</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+		</div>
+
+		</footer>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,37 @@
+<!--NAV BAR-->
+
+      <div class="masthead">
+	        <nav class="navbar fixed-top navbar-expand-md navbar-light bg-light rounded mb-3">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav text-md-center nav-justified w-100">
+              <li class="nav-item active">
+                <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="services.php">Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="careers.php">Careers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.php">Contact</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.php">About Us</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+<!-- NAVBAR-->
